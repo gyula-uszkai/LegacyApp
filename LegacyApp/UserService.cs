fee4655b471cf6d3d342d38cac9cd1b9b49f3d7d
gyula.uszkai <gyula.uszkai@dotwhite.com>
2023-04-05 10:11:25 +0300

    Extract User validator

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -4,35 +4,35 @@ namespace LegacyApp
 {
     public class UserService
     {
+        private readonly IUserValidator userValidator;
+
+        public UserService() : this(new UserValidator())
+        {
+        }
+
+        public UserService(IUserValidator userValidator)
+        {
+            this.userValidator = userValidator;
+        }
+
         public bool AddUser(string firname, string surname, string email, DateTime dateOfBirth, int clientld)
         {
-            if (string.IsNullOrEmpty(firname) || string.IsNullOrEmpty(surname))
-            {
-                return false;
-            }
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-            if (age < 21)
-            {
-                return false;
-            }
-
-            var clientRepository = new ClientRepository();
-            var client = clientRepository.GetById(clientld);
-
             var user = new User
             {
-                Client = client,
                 DateOfBirth = dateOfBirth,
                 EmailAddress = email,
                 Firstname = firname,
                 Surname = surname
             };
+
+            if (!userValidator.IsUserValid(user))
+            {
+                return false;
+            }
+
+            var client = GetClient(clientld);
+            user.Client = client;
+
 
             if (client.Name == "VeryImportantClient")
             {
@@ -68,6 +68,13 @@ namespace LegacyApp
             UserDataAccess.AddUser(user);
 
             return true;
+        }
+
+        private static Client GetClient(int clientld)
+        {
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(clientld);
+            return client;
         }
     }
 }
---
+++ LegacyApp/UserValidator.cs
@@ -0,0 +1,43 @@
+﻿namespace LegacyApp
+{
+    public class UserValidator : IUserValidator
+    {
+        private const int AgeLimit = 21;
+
+        public bool IsUserValid(User user)
+        {
+            return IsNameValid(user) && IsEmailValid(user) && IsAgeValid(user);
+        }
+
+        private bool IsNameValid(User user)
+        {
+            return !string.IsNullOrEmpty(user.Firstname) && !string.IsNullOrEmpty(user.Surname);
+        }
+
+        private bool IsEmailValid(User user)
+        {
+            // Consider using regex or other pattern matching
+            return user.EmailAddress.Contains("@") || user.EmailAddress.Contains(".");
+        }
+
+        private bool IsAgeValid(User user)
+        {
+            int age = CalculateAge(user);
+            return age >= AgeLimit;
+        }
+
+        private static int CalculateAge(User user)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - user.DateOfBirth.Year;
+            bool isBirthDayLaterThisYear = now.Month < user.DateOfBirth.Month ||
+                                           (now.Month == user.DateOfBirth.Month && now.Day < user.DateOfBirth.Day);
+            if (isBirthDayLaterThisYear)
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
