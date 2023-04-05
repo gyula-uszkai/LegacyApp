be88c75775d992ed9b0388bb22a067869914caee
gyula.uszkai <gyula.uszkai@dotwhite.com>
2023-04-05 10:21:27 +0300

    Add fake test

---
+++ LegacyAppTests1/UserServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace LegacyApp.Tests
+{
+    public class UserServiceTests
+    {
+        [Fact()]
+        public void AddUserTest_Executes()
+        {
+            var userService = new UserService();
+            var result = userService.AddUser("John", "Doe", "john@doe.com", new DateTime(1993, 1, 1), 4);
+            Assert.True(true, "This test needs an implementation");
+        }
+    }
+}
